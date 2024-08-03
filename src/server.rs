7e40b0c794cc145b6e419074489d9c25f60c6b1d
mlaskowski7 <mtlaskowski7@gmail.com>
2024-08-03 16:30:44 +0200

    finished working on server.rs

--- src/server.rs
+++ src/server.rs
@@ -59,5 +59,106 @@ impl ChatServer {
         }
     }
 
-    fn send_message(&self, room: &str, message: &str)
+    fn send_message(&self, room: &str, message: &str, skip_id: usize) {
+        if let Some(sessions) = self.rooms.get(room){
+            for id in sessions {
+                if *id != skip_id {
+                    if let Some(addr) = self.sessions.get(id) {
+                        addr.do_send(Message(message.to_owned()));
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl Actor for ChatServer {
+    type Context = Context<Self>;
+}
+
+impl Handler<Connect> for ChatServer {
+    type Result = usize;
+    fn handle(&mut self, msg: Connect, _: &mut Self::Context) -> Self::Result {
+        let id = self.rng.gen::<usize>();
+        self.sessions.insert(id, msg.addr);
+        self.rooms
+            .entry("main".to_string())
+            .or_insert_with(HashSet:new)
+            .insert(id);
+        self.send_message("main", &json!({
+            "value" : vec![format!("{}", id)],
+            "chat_type": session::ChatType::CONNECT
+        }).to_string(), 0);
+
+        id
+    }
+}
+
+impl Handler<Disconnect> for ChatServer {
+    type Result = ();
+    fn handle(&mut self, msg: Disconnect, _: &mut Self::Context) -> Self::Result {
+        let mut rooms: Vec<String> = vec![];
+
+        if self.sessions.remove(&msg.id).is_some() {
+            for (name, sessions) in &mut self.rooms {
+                if sessions.remove(&msg.id) {
+                    rooms.push(name.to_owned());
+                }
+            }
+        }
+
+        for room in rooms {
+            self.send_message("main", &json!({
+                "room": room,
+                "value": vec![format!("Someone has disconnected!")],
+                "chat_type": session::ChatType::DISCONNECT
+            }).to_string(), 0);
+        }
+    }
+}
+
+impl Handler<ClientMessage> for ChatServer {
+    type Result = ();
+
+    fn handle(&mut self, msg: ClientMessage, _: &mut Self::Context) -> Self::Result {
+        self.send_message(&msg.room, &msg.msg, msg.id);
+    }
+}
+
+impl Handler<ListRooms> for ChatServer {
+    type Result = MessageResult<ListRooms>;
+    fn handle(&mut self, msg: ListRooms, _: &mut Self::Context) -> Self::Result {
+        let mut rooms = vec![];
+        for key in self.rooms.keys() {
+            rooms.push(key.to_owned());
+        }
+
+        MessageResult(rooms)
+    }
+}
+
+impl Handler<Join> for ChatServer {
+    type Result = ();
+    fn handle(&mut self, msg: Join, _: &mut Self::Context) -> Self::Result {
+        let Join {id, name} = msg;
+        let mut rooms = vec![];
+        for (n, sessions) in &mut self.rooms {
+            if sessions.remove(&id) {
+                rooms.push(n.to_owned());
+            }
+        }
+
+        for room in rooms {
+            self.send_message(&room, &json!({
+                "room": room,
+                "value": vec!["Someone has disconnected!".to_string()],
+                "chat_type":  session::ChatType::DISCONNECT
+            }).to_string(),0);
+        }
+
+        self.rooms
+            .entry(name.clone())
+            .or_insert_with(HashSet::new)
+            .insert(id);
+    }
 }
