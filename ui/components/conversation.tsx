8379fc6dafd41ccf00a747de718cdc0ac5e51785
mlaskowski7 <mtlaskowski7@gmail.com>
2024-08-04 22:10:50 +0200

    created websocket hook

---
+++ ui/components/conversation.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useRef } from "react";
+import Avatar from "./avatar";
+
+interface ConversationItemProps {
+  right: boolean;
+  content: string;
+  username: string;
+}
+
+function ConversationItem({ right, content, username }: ConversationItemProps) {
+  if (right) {
+    return (
+      <div className="w-full flex justify-end">
+        <div className="flex gap-3 justify-end">
+          <div className="max-w-[65%] bg-violet-500 p-3 text-sm rounded-xl rounded-br-none">
+            <p className="text-white">{content}</p>
+          </div>
+          <div className="mt-auto">
+            <Avatar>{username}</Avatar>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="flex gap-3 w-full">
+      <div className="mt-auto">
+        <Avatar color="rgb(245 158 11)">{username}</Avatar>
+      </div>
+      <div className="max-w-[65%] bg-gray-200 p-3 text-sm rounded-xl rounded-bl-none">
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+}
+
+interface ConversationProps {
+  data: Array<{
+    id: string;
+    user_id: string;
+    content: string;
+  }>;
+  auth: {
+    id: string;
+  };
+  users: Map<string, string>;
+}
+
+export default function Conversation({ data, auth, users }: ConversationProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    ref.current?.scrollTo(0, ref.current.scrollHeight);
+  }, [data]);
+  return (
+    <div className="p-4 space-y-4 overflow-auto" ref={ref}>
+      {data.map((item) => {
+        return (
+          <ConversationItem
+            right={item.user_id === auth.id}
+            content={item.content}
+            username={users.get(item.user_id) || "Unknown user"}
+            key={item.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
---
+++ ui/libs/websocket.ts
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from "react";
+
+type OnMessageHandler = (messsage: string) => void;
+
+export default function useWebsocket(onMessage: OnMessageHandler) {
+  const ws = useRef<WebSocket | null>(null);
+  useEffect(() => {
+    if (ws.current !== null) return;
+    const wsUri = "ws://localhost:8080/ws";
+    ws.current = new WebSocket(wsUri);
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onclose = () => console.log("ws closed");
+    const wsCurrent = ws.current;
+    return () => {
+      wsCurrent.close();
+    };
+  }, []);
+  useEffect(() => {
+    if (!ws.current) return;
+    ws.current.onmessage = (e) => {
+      onMessage(e.data);
+    };
+  }, []);
+  const sendMessage = (msg: string) => {
+    if (!ws.current) return;
+    ws.current.send(msg);
+  };
+  return sendMessage;
+}
